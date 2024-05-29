f8461ef470936cc6a8445b9aed819526600a41e7
Sacalito <sacalito@gmail.com>
2024-05-29 19:17:05 +0000

    edited run.sh file

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #Install docker
 export DEBIAN_FRONTEND=noninteractive
 apt update && apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
